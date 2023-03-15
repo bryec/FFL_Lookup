49a68eb1d281835434acaa553abb3586af40dec9
bryec <127230315+bryec@users.noreply.github.com>
2023-03-15 12:12:06 -0500

    change to mysql

---
+++ testGeo.py
@@ -0,0 +1,17 @@
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderUnavailable, GeocoderTimedOut
+
+geolocator = Nominatim(user_agent="my_ffl_lookup")
+
+def geocode_address(address):
+    try:
+        location = geolocator.geocode(address)
+        return (location.latitude, location.longitude) if location else (None, None)
+    except (GeocoderUnavailable, GeocoderTimedOut) as e:
+        print(f"Error: {e}")
+        return (None, None)
+
+if __name__ == "__main__":
+    test_address = "4 CALLE COLON SUITE 3, AGUADA, PR, 00602"
+    lat, lng = geocode_address(test_address)
+    print(f"Latitude: {lat}, Longitude: {lng}")
