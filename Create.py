09b683ab0dd8ee35f189d927d5ea78e7a1c978d1
bryec <127230315+bryec@users.noreply.github.com>
2023-03-15 11:51:04 -0500

    change to mysql

--- Create.py
+++ Create.py
@@ -1,45 +1,5 @@
-import mysql.connector
 import csv
-
-db = mysql.connector.connect(
-    host="localhost",
-    user="ffl",
-    password="ffladdresslookup",
-    database="FFL_Address"
-)
-cursor = db.cursor()
-
-
-def update_database(data):
-    for row in data:
-        while len(row) < 17:  # Fill missing columns with empty strings
-            row.append('')
-
-        try:
-            cursor.execute("""
-                INSERT INTO store_data VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ON DUPLICATE KEY UPDATE
-                LIC_NAME=%s,
-                LIC_ISSU_DATE=%s,
-                LIC_EXP_DATE=%s,
-                MAIL_LINE1=%s,
-                MAIL_LINE2=%s,
-                MAIL_CITY=%s,
-                MAIL_STATE=%s,
-                MAIL_ZIP=%s,
-                PREMISE_NAME=%s,
-                PREMISE_STREET=%s,
-                PREMISE_CITY=%s,
-                PREMISE_STATE=%s,
-                PREMISE_ZIP=%s,
-                PREMISE_PHONE=%s
-                """, row + row[:15])
-            print(f"Updated entry for {row[1]}")
-        except mysql.connector.Error as e:
-            print(f"Error updating entry for {row[1]}: {e}")
-
-        db.commit()
-
+import mysql.connector
 
 def read_data_from_file(filename):
     data = []
@@ -50,8 +10,23 @@ def read_data_from_file(filename):
             data.append(row)
     return data
 
-
 data = read_data_from_file('monthly_data.txt')
 
-if __name__ == "__main__":
-    update_database(data)
+db = mysql.connector.connect(
+    host="localhost",
+    user="ffl",
+    password="ffladdresslookup",
+    database="FFL_Address"
+)
+cursor = db.cursor()
+
+for row in data:
+    # Assuming columns in the table are: col1, col2, col3, ..., col16
+    # Adjust the number of columns and their names as needed.
+    query = f"""INSERT INTO table_name (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    cursor.execute(query, row)
+
+db.commit()
+cursor.close()
+db.close()
